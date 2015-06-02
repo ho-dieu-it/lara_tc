89c7195eeebebcb86a8de89a5451efddd2c7ff98
ho-dieu-it <hodieu.it@gmail.com>
2015-06-02 18:38:09 +0700

    Problem $.when.apply.

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -176,7 +176,40 @@
 	</div>
 </div>
 <script type="text/javascript">
-            					console.log($('#fileupload'));
+//1. Declare an array of 4 elements
+var data = [1,2,3,4]; // the ids coming back from serviceA
+// 2. Declare an array of Deferred objects
+var processItemsDeferred = [];
+
+// 3. For each element of data, create a Deferred push push it to the array
+for(var i = 0; i < data.length; i++){
+  processItemsDeferred.push(processItem(data[i]));
+}
+
+// 4. WHEN ALL Deferred objects in the array are resolved THEN call the function
+//    Note : same as $.when(processItemsDeferred[0], processItemsDeferred[1], ...).then(everythingDone);
+$.when.apply($, processItemsDeferred).then(everythingDone); 
+
+// 3.1. Function called by the loop to create a Deferred object (data is numeric)
+function processItem(data) {
+  // 3.1.1. Create the Deferred object and output some debug
+  var dfd = $.Deferred();
+  console.log('called processItem');
+  
+
+  // 3.1.2. After some timeout, resolve the current Deferred
+  //in the real world, this would probably make an AJAX call.
+  setTimeout(function() { dfd.resolve() }, 2000);    
+
+  // 3.1.3. Return that Deferred (to be inserted into the array)
+  return dfd.promise();
+}
+
+// 4.1. Function called when all deferred are resolved
+function everythingDone(){
+  // 4.1.1. Do some debug trace
+  console.log('processed all items');
+}
 //$('.dropdown-toggle').dropdown();
     $(".dropdown-menu li a").click(function(){
 			$val=$(this).text();
